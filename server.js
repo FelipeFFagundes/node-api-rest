bbc21bf9fdce53399627e1bf43d106d0cbafcf24
felipeffagundes <ffagundes1234@gmail.com>
2022-09-16 15:23:07 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require("http")
+const port = 3000;
+
+const rotas = { 
+    '/' : 'Curso de Node',
+    '/livros': 'Pagina de Livros',
+    '/autores': 'Listagem de Autores',
+    '/editora': 'Pagina da editora'
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(rotas[req.url]);
+})
+
+server.listen(port, () =>{
+    console.log(`to ouvindo carai, em http://localhost:${port}`);
+})
